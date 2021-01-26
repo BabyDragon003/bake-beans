c3abec6ac64f48339db69a98a2d43412932976a4
BabyDragon003 <golddragon01234@gmail.com>
2021-01-26 16:21:52 +0900

    "2021-01-26 16:21"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
+import Box from "@mui/material/Box";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Box paddingY={6} paddingX={2}>
-        <Home />
-      </Box>
-    </BrowserRouter>
   );
 }
 

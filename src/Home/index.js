ab1d1cd485dacf3af1a311730c20b36018e1f5ec
BabyDragon003 <golddragon01234@gmail.com>
2021-05-05 16:21:52 +0900

    "2021-05-05 16:21"

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,17 +8,6 @@ import ReferralLink from "./components/ReferralLink";
 import { useAuthContext } from "../providers/AuthProvider";
 import Footer from "./components/Footer";
 
-const Wrapper = styled("div")(({ theme }) => ({
-  maxWidth: 400,
-  margin: "0 auto",
-  [theme.breakpoints.down("sm")]: {
-    maxWidth: "100%",
-  },
-}));
-
-export default function Home() {
-  const { address } = useAuthContext();
-
   return (
     <Wrapper>
       <Connect />

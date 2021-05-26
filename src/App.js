dc54917a6030541610e2e3a07dd98a1135112f7b
BabyDragon003 <golddragon01234@gmail.com>
2021-05-26 16:21:52 +0900

    "2021-05-26 16:21"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Box paddingY={6} paddingX={2}>
+        <Home />
+      </Box>
+    </BrowserRouter>
   );
 }
 
